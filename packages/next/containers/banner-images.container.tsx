90110ef1d42052626338ab38bc20c394c613eef4
miaowing <me@mxb.cc>
2020-12-22 11:35:23 +0800

    fix banner images

--- packages/next/containers/banner-images.container.tsx
+++ packages/next/containers/banner-images.container.tsx
@@ -4,7 +4,7 @@ import { GET_BANNER_IMAGES } from "../graphql/banner.gql";
 import styles from "./banner-images.conainer.module.less";
 
 export const BannerImagesContainer = props => {
-    const style = ['one', 'two', 'three', 'four'];
+    const style = [styles.one, styles.two, styles.three, styles.four];
     const { error, data } = useQuery(GET_BANNER_IMAGES);
     if (error) {
         return <div>{error}</div>;
@@ -13,13 +13,14 @@ export const BannerImagesContainer = props => {
     const images = data?.allBannerImages ?? [];
     return <div className={styles.images} style={{ height: images.length !== 0 ? 543 : 'auto' }}>
         {images.map((image, index) => <div key={index} className={`${styles.container} ${style[index]}`}>
-            <div className={`${styles.layer} ${image?.content ? styles.has_content : ''}`}>
-                <img src={image?.image?.publicUrl} alt={index}/>
-                {image?.content ?
-                    <div className={styles.content} dangerouslySetInnerHTML={{ __html: image?.content }}/> :
-                    ''
-                }
+                <div className={`${styles.layer} ${image?.content ? styles.has_content : ''}`}>
+                    <div className={styles.img} style={{ backgroundImage: `url(${image?.image?.publicUrl})` }}/>
+                    {image?.content ?
+                        <div className={styles.content} dangerouslySetInnerHTML={{ __html: image?.content }}/> :
+                        ''
+                    }
+                </div>
             </div>
-        </div>)}
+        )}
     </div>
 }
