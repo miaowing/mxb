8a5a91149bda88bdba865e253dc7e07f2fc93ce6
miaowing <me@mxb.cc>
2021-02-04 16:36:46 +0800

    remove link category

--- packages/next/pages/links.tsx
+++ packages/next/pages/links.tsx
@@ -7,28 +7,9 @@ import { BaseProps } from "../interfaces/props.interface";
 import { Query } from "../components/query.component";
 import { Links, LinkItem } from "../components/link.component";
 import { GET_INNER_LINKS } from "../graphql/links.gql";
-import { Link } from "../interfaces/link.interface";
-import { Tag } from "../interfaces/tag.interface";
 import { CommentContainer } from "../containers/comment.container";
 
 export default function LinksPage({ meta }: BaseProps) {
-    const handleLinks = (links: Link[]) => {
-        const map = new Map<string, Tag & { links: Link[] }>();
-        links.forEach(link => {
-            link.tags.forEach(tag => {
-                if (!map.has(tag.key)) {
-                    map.set(tag.key, { ...tag, links: [] });
-                }
-                map.get(tag.key).links.push(link);
-            })
-        });
-        const categories = [...map.values()];
-        categories.sort((a, b) =>
-            a.sort > b.sort ? 1 : a.sort < b.sort ? -1 : 0,
-        );
-        return categories;
-    }
-
     return <>
         <Layout>
             <Head>
@@ -36,18 +17,16 @@ export default function LinksPage({ meta }: BaseProps) {
             </Head>
             <Header title={meta.title} avatar={meta?.avatar?.publicUrl}/>
             <div style={{ maxWidth: 1400, margin: '0 auto' }}>
-                <Query query={GET_INNER_LINKS} render={links => {
-                    return handleLinks(links).map(category => <Links key={category.key}>
-                        {category.links.map(link => <LinkItem
-                            key={link.name}
-                            title={link.name}
-                            url={link.url}
-                            thumb={link?.avatar?.publicUrl}
-                            description={link.description}
-                            category={category.name}
-                        />)}
-                    </Links>)
-                }}/>
+                <Query query={GET_INNER_LINKS} render={links => <Links>
+                    {links.map(link => <LinkItem
+                        key={link.name}
+                        title={link.name}
+                        url={link.url}
+                        thumb={link?.avatar?.publicUrl}
+                        description={link.description}
+                        category={link?.tags?.map(tag => <span className="mr-2">{tag.name}</span>)}
+                    />)}
+                </Links>}/>
                 <div className="mx-8 md:mx-16 mt-16">
                     <CommentContainer page="/links" meta={meta}/>
                 </div>
