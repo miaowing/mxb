2a84f9efb03a502f1a23ffdfb117fff013bfe112
miaowing <me@mxb.cc>
2021-02-05 11:37:43 +0800

    fix config.example

--- packages/keystone/config.example.ts
+++ packages/keystone/config.example.ts
@@ -1,14 +1,22 @@
 const env = process.env ?? {} as any;
 
-export const playlistId = env.PLAYLIST_ID ?? '127474627';
-export const neteasePhone = env.NETEASE_PHONE ?? '';
-export const neteasePassword = env.NETEASE_PASSWORD ?? '';
-export const neteaseCountryCode = env.NETEASE_COUNTRY_CODE ?? '86';
-
 export const port = env.PORT ?? 3000;
 export const mongoUri = env.MONGO_URI ?? 'mongodb://localhost:27017/mxb-cc';
 export const externalUrl = env.EXTERNAL_URL ?? 'https://mxb.cc';
 export const sessionStore = env.SESSION_STORE ?? 'memory';
+export const redisHost = env.REDIS_HOST ?? 'localhost';
+export const redisPort = env.REDIS_PORT ?? '6379';
+export const redisPassword = env.REDIS_PORT;
+export const redisDatabase = env.REDIS_DB ?? 0;
+
+export const playlistId = env.PLAYLIST_ID ?? '127474627';
+export const neteasePhone = env.NETEASE_PHONE ?? '';
+export const neteasePassword = env.NETEASE_PASSWORD ?? '';
+export const neteaseCountryCode = env.NETEASE_COUNTRY_CODE ?? '86';
+export const singUsername = env.SING_USERNAME;
+export const singPassword = env.SING_PASSWORD;
+
+
 export const mailer = {
     host: env.MAILER_HOST ?? 'smtp.mxhichina.com',
     port: env.MAILER_PORT ?? 465,
